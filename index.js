098ca6bfa2a9ee670c2267ec1d79843281c10ff6
Amir Val <amir.val715@gmail.com>
2025-05-13 21:26:35 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var str = "hi from vim again: ";
+alert(str);
+function welcom() {
+let intredus = "I'm a developer";
+console.log(intredus);
+document.getElementById("demo").innerHTML = intredus;
+}
+welcom();
+function changeColor() {
+    document.getElementById("limo").style = "background-color: red;";
+}
+changeColor();
